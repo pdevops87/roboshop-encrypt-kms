897f33d979af3c403e99cfebcd45c531b8413ed1
Your Name <vemasani.pavani@gmail.com>
2026-06-11 16:26:39 +0530

    encrypt an ec2 server ebs volume disk through kms

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "instance" {
   ami           = var.ami
-  instance_type = "t3.small"
+  instance_type = "t3.micro"
  vpc_security_group_ids = ["sg-00200ecf86b9b6868"]
   root_block_device {
     encrypted = true
--- variables.tf
+++ variables.tf
@@ -1,6 +1,11 @@
 variable "ami"{
-  ami = "ami-0220d79f3f480ecf5"
+
 }
 variable "kms" {
-  kms = "arn:aws:kms:us-east-1:041445559784:key/89d045f5-7a12-4a16-86d3-e90719e64048"
+
 }
+
+
+
+ami = "ami-0220d79f3f480ecf5"
+kms = "arn:aws:kms:us-east-1:041445559784:key/89d045f5-7a12-4a16-86d3-e90719e64048"
