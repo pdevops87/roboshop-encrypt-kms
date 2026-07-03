423a702d5513aa26eec7ec32a1c2818e6fc1d537
Your Name <vemasani.pavani@gmail.com>
2026-07-03 20:53:03 +0530

    kms key

--- variables.tf
+++ variables.tf
@@ -2,9 +2,10 @@ variable "ami"{
 default = "ami-0220d79f3f480ecf5"
 }
 variable "kms" {
-default = "arn:aws:kms:us-east-1:041445559784:key/89d045f5-7a12-4a16-86d3-e90719e64048"
+default = "arn:aws:kms:us-east-1:041445559784:key/bbcec40c-e637-45dd-8f1a-8fce019cf076"
 }
 
 variable "env" {
   default = "dev"
 }
+
